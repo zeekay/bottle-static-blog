cc9a9122d3b625e2f36255acd71107621e749dd6
zeekay <zeekayy@gmail.com>
2011-12-11 20:04:11 -0600

    PEP8

--- blog.py
+++ blog.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
-import argparse, glob, os
+import argparse
+import glob
+import os
 from bottle import Bottle, LocalRequest, run, template
 import misaka
 
-# monkey patch LocalRequest
-LocalRequest.environ = {}
-
-app = Bottle()
-
 GENERATED_DIR = 'generated/'
 POSTS_DIR = 'posts/'
+
+# monkey patch LocalRequest
+LocalRequest.environ = {}
+app = Bottle()
+
 
 def list_posts():
     posts = []
@@ -18,22 +20,29 @@ def list_posts():
         posts.append(name)
     return posts
 
+
 def cache_routes(app):
     def cache_static(route):
         path = os.path.join(GENERATED_DIR, route.rule[1:])
-        try: os.mkdir(path)
-        except OSError: pass
+        try:
+            os.mkdir(path)
+        except OSError:
+            pass
         with open(os.path.join(path, 'index.html'), 'w') as html:
             html.write(route.call())
 
     def cache_posts(route):
         posts_path = os.path.join(GENERATED_DIR, 'posts')
-        try: os.mkdir(posts_path)
-        except OSError: pass
+        try:
+            os.mkdir(posts_path)
+        except OSError:
+            pass
         for md in list_posts():
             path = os.path.join(posts_path, md)
-            try: os.mkdir(path)
-            except OSError: pass
+            try:
+                os.mkdir(path)
+            except OSError:
+                pass
             with open(os.path.join(path, 'index.html'), 'w') as html:
                 html.write(route.call(post=md))
 
@@ -43,10 +52,12 @@ def cache_routes(app):
         else:
             cache_static(route)
 
+
 @app.get('/')
 def index():
     posts = dict((name, app.get_url('posts', post=name)) for name in list_posts())
     return template('index', posts=posts)
+
 
 @app.get('/posts/:post/', name='posts')
 def posts(post):
